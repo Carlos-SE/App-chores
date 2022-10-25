0c6e674088e326bc2f038ff533a2fc9adfd8eaac
luistf24 <luis.tormo24@gmail.com>
2022-10-25 10:38:43 +0200

    Avanzo en el milestone0 con la cración de la clase song

---
+++ app/controllers/songController.js
@@ -0,0 +1,36 @@
+class Song {
+	constructor(titulo, artista, disco, categoria, duracion, nombre) {
+		this.titulo		= titulo;
+		this.artista	= artista;
+		this.disco		= disco;
+		this.categoria 	= categoria; 
+		this.duracion	= duracion;
+		this.nombre 	= nombre;
+	}
+
+	getTitulo() {
+		return this.titulo;
+	}
+	
+	getArtista() {
+		return this.artista;
+	}
+
+	getDisco() {
+		return this.disco
+	}
+
+	getCategoria() {
+		return this.categoria;
+	}
+	
+	getDuracion() {
+		return this.duracion;
+	}
+
+	getNombre() {
+		return this.nombre;
+	}
+}
+
+module.exports = Song 
