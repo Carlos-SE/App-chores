69967df474e1537fe91177dcd9a721e2bf818878
luistf24 <luis.tormo24@gmail.com>
2022-10-30 17:11:58 +0100

    Avanzo en #2 implementando los métodos para obtener los atributos de la clase Song Además también he implementando un test para probar lo hecho hasta ahora y he modificado el iv.yaml con lo implementado

--- app/lib/song.js
+++ app/lib/song.js
@@ -7,6 +7,49 @@ class Song {
 		this.duracion	= duracion;
 		this.archivo	= archivo;
 	}
+	
+
+	/**
+	* @return:	Devuelve el título de la canción
+ 	*/
+	getTitulo() {
+		return this.titulo;
+	}
+
+	/**
+	* @return: Devuelve el artista de la canción
+ 	*/
+	getArtista() {
+		return this.artista;
+	}
+
+	/**
+	* @return: Devuelve el disco donde está la canción
+ 	*/
+	getDisco() {
+		return this.disco;
+	}
+
+	/**
+	* @return: Devuelve la categoría de la canción
+ 	*/
+	getCategoria() {
+		return this.categoria;
+	}
+	
+	/**
+	* @return: Devuelve la duración de la canción
+ 	*/
+	getDuracion() {
+		return this.duracion;
+	}
+
+	/**
+	* @return: Devuelve el archivo de la canción
+ 	*/
+	getArchivo() {
+		return this.archivo;
+	}
 }
 
 module.exports = Song
---
+++ app/test_1.js
@@ -0,0 +1,12 @@
+
+const Song	= require('./lib/song.js')
+
+const song	= new Song('Ace of Spades', 'Motorhead', 'Ace of Spades (Expanded Edition)', 
+						'Speed metal', '2:46 min', 'ace_of_spades.mp3');
+
+
+// Tests:
+
+console.log(('Titulo: '+song.getTitulo() + ', Artista:'+song.getArtista() 
+				+ ', Disco:'+song.getDisco() + ', Categoria:' + song.getCategoria() 
+				+ ', Duracion:' + song.getDuracion() + ', Nombre del archivo:' + song.getArchivo()));
