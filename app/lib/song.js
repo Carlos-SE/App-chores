daa60df8a2cf7ad8602c500ca94636afafb65477
luistf24 <luis.tormo24@gmail.com>
2022-11-03 17:29:41 +0100

    Termino con #12
    Implemento los distintos métodos para obtener los atributos y amplio el test

--- app/lib/song.js
+++ app/lib/song.js
@@ -9,6 +9,66 @@ class Song {
 		this.archivo	= archivo;
 	}
 
+	
+	/**
+	* @return: Título de la canción
+ 	*/
+	getTitulo() {
+		return this.titulo;
+	}
+	
+	/**
+	* @return: Artista o artistas de la canción
+ 	*/
+	getArtista() {
+		return this.artista;
+	}
+
+	/**
+	* @return: Disco al que pertenece la canción
+ 	*/
+	getDisco() {
+		return this.disco
+	}
+
+	/**
+	* @return: Categoría a la que pertenece la canción
+ 	*/
+	getCategoria() {
+		return this.categoria;
+	}
+	
+	/**
+	* @return: Duración de la canción
+ 	*/
+	getDuracion() {
+		return this.duracion;
+	}
+
+	/**
+	* @return: Archivo de la canción
+ 	*/
+	getArchivo() {
+		return this.archivo;
+	}
+
+
+	/**
+	* @return: Json que contiene los atributos
+ 	*/
+	getJson() {
+		var song = {
+			titulo:		this.titulo,
+			artista:	this.artista,
+			disco:		this.disco,
+			categoria:	this.categoria,
+			duracion:	this.duracion,
+			archivo:	this.archivo,
+		}
+
+		return song;
+	}
+
 }
 
 
--- app/test_1.js
+++ app/test_1.js
@@ -5,3 +5,9 @@ const song	= new Song('Ace of Spades', ['Motorhead'], 'Ace of Spades (Expanded E
 						'Speed metal', '2:46 min', 'ace_of_spades.mp3');
 
 
+// Tests:
+
+console.log(song.getJson());
+console.log(('Titulo: '+song.getTitulo() + ', Artista:'+song.getArtista() 
+				+ ', Disco:'+song.getDisco() + ', Categoria:' + song.getCategoria() 
+				+ ', Duracion:' + song.getDuracion() + ', Nombre del archivo:' + song.getArchivo()));
