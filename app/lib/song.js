325ba35692edcdc185f5051b18a8190d97140724
luistf24 <luis.tormo24@gmail.com>
2022-11-03 17:18:09 +0100

    Avanzo en #12
    Creo clase Song, modifico iv.yaml y añado test

---
+++ app/lib/song.js
@@ -0,0 +1,15 @@
+
+class Song {
+	constructor(titulo, artista, disco, categoria, duracion, archivo) {
+		this.titulo		= titulo;
+		this.artista	= artista;
+		this.disco		= disco;
+		this.categoria	= categoria; 
+		this.duracion	= duracion;
+		this.archivo	= archivo;
+	}
+
+}
+
+
+module.exports = Song 
---
+++ app/test_1.js
@@ -0,0 +1,7 @@
+
+const Song	= require('./lib/song.js')
+
+const song	= new Song('Ace of Spades', ['Motorhead'], 'Ace of Spades (Expanded Edition)', 
+						'Speed metal', '2:46 min', 'ace_of_spades.mp3');
+
+
