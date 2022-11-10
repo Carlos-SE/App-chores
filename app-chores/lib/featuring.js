028f4d25405db3c3d034f6a23b44825d16cce476
luistf24 <luis.tormo24@gmail.com>
2022-11-10 19:15:10 +0100

    Añado getters de Featuring, closes #13
    Implemento funciones para obtener los atributos de la clase Featuring
    Termino la solución de #13

--- app-chores/lib/featuring.js
+++ app-chores/lib/featuring.js
@@ -4,6 +4,28 @@ class Featuring {
 		this.colaboradores	= colaboradores;
 		this.cancion		= cancion;
 	}
+	
+
+	/**
+	* @return: Artista principal de la colaboración
+ 	*/
+	getPrincipal() {
+		return this.principal;
+	}
+
+	/**
+	* @return: Artista/s colaboradores de principal en la canción
+ 	*/
+	getColaboradores() {
+		return this.colaboradores;
+	}
+
+	/**
+	* @return: Canción de la colaboración
+ 	*/
+	getCancion() {
+		return this.cancion;
+	}
 
 }
 
