4551a5063912f74b1ec324b77de1f0fdba696db2
luistf24 <luis.tormo24@gmail.com>
2022-11-10 19:01:22 +0100

    Inicio implementación de la clase Featuring #13
    Una vez aclarado #16 empiezo con el desarrollo de la clase Featuring.
    Esta clase tendrá los siguientes atributos:
    - Principal: Artista principal de la colaboración
    - colaboradores: Artista/s que han colaborado en una canción con principal
    - cancion: Canción en la que han colaborado principal y colaboradores
    
    Además se ha modificado iv.yaml para añadir la nueva entidad

---
+++ app-chores/lib/featuring.js
@@ -0,0 +1,10 @@
+class Featuring {
+	constructor(principal, colaboradores, cancion) {
+		this.principal 		= principal;
+		this.colaboradores	= colaboradores;
+		this.cancion		= cancion;
+	}
+
+}
+
+module.exports = Featuring
