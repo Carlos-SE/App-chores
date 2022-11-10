1dc4f0947ae0f8c3011561d45b93096cb9b92e28
luistf24 <luis.tormo24@gmail.com>
2022-11-10 22:05:29 +0100

    fix: Elimino los getters que he creado en Song
    En Javascript los getters son automáticos.
    También corrijo fallo en la función getJson devido a renombrar
    el atributo artista a interpretes

--- app-chores/lib/song.js
+++ app-chores/lib/song.js
@@ -8,61 +8,18 @@ class Song {
 		this.archivo		= archivo;
 	}
 
-	
-	/**
-	* @return: Título de la canción
- 	*/
-	getTitulo() {
-		return this.titulo;
-	}
-	
-	/**
-	* @return: Artista o artistas de la canción
- 	*/
-	getArtista() {
-		return this.artista;
-	}
-
-	/**
-	* @return: Género de la canción
- 	*/
-	getGenero() {
-		return this.genero;
-	}
-
-	/**
-	* @return: Disco al que pertenece la canción
- 	*/
-	getDisco() {
-		return this.disco
-	}
-
-	/**
-	* @return: Duración de la canción
- 	*/
-	getDuracion() {
-		return this.duracion;
-	}
-
-	/**
-	* @return: Archivo de la canción
- 	*/
-	getArchivo() {
-		return this.archivo;
-	}
-
 
 	/**
 	* @return: Json que contiene los atributos
  	*/
 	getJson() {
 		var song = {
-			titulo:		this.titulo,
-			artista:	this.artista,
-			disco:		this.disco,
-			categoria:	this.categoria,
-			duracion:	this.duracion,
-			archivo:	this.archivo,
+			titulo:			this.titulo,
+			interpretes:	this.interpretes,
+			disco:			this.disco,
+			categoria:		this.categoria,
+			duracion:		this.duracion,
+			archivo:		this.archivo,
 		}
 
 		return song;
