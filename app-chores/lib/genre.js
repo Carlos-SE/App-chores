0107a1a4161da161b3428b6ed6b65bd33212ae2c
luistf24 <luis.tormo24@gmail.com>
2022-11-10 20:35:17 +0100

    feat: Añado getters de Genre #14
    Añado funciones para obtener los atributos(genero, canciones) de Genre

--- app-chores/lib/genre.js
+++ app-chores/lib/genre.js
@@ -4,6 +4,20 @@ class Genre {
 		this.canciones	= canciones;
 	}
 
+
+	/**
+	* @return: String con el género
+ 	*/
+	getGenero() {
+		return this.genero;
+	}
+
+	/**
+	* @return: Listas de canciones asociadas al género
+ 	*/
+	getCanciones() {
+		return this.canciones;
+	}
 }
 
 module.exports = Genre;
