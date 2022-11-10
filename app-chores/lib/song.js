c9cb23e3d70a70416590c162f08c8b4e6a74c1e9
luistf24 <luis.tormo24@gmail.com>
2022-11-10 17:09:32 +0100

    feat: Añado getters a la clase Song #15
    Implemento los getters para obtener los distintos atributos de la clase Song
    avanzando en #15

--- app-chores/lib/song.js
+++ app-chores/lib/song.js
@@ -7,6 +7,50 @@ class Song {
 		this.duracion	= duracion;
 		this.archivo	= archivo;
 	}
+
+	
+	/**
+	* @return: Título de la canción
+ 	*/
+	getTitulo() {
+		return this.titulo;
+	}
+	
+	/**
+	* @return: Artista o artistas de la canción
+ 	*/
+	getArtista() {
+		return this.artista;
+	}
+
+	/**
+	* @return: Género de la canción
+ 	*/
+	getGenero() {
+		return this.genero;
+	}
+
+	/**
+	* @return: Disco al que pertenece la canción
+ 	*/
+	getDisco() {
+		return this.disco
+	}
+
+	/**
+	* @return: Duración de la canción
+ 	*/
+	getDuracion() {
+		return this.duracion;
+	}
+
+	/**
+	* @return: Archivo de la canción
+ 	*/
+	getArchivo() {
+		return this.archivo;
+	}
+
 }
 
 module.exports = Song 
