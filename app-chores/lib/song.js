7de526ee75ba216d415776ad55530b6ab864b57b
luistf24 <luis.tormo24@gmail.com>
2022-11-10 16:59:37 +0100

    Empiezo la implementación de la clase Song #15
    Después de ver #15, tenemos que implementar las canciones a la aplicación
    para ello se ha implementado la clase Song en el archivo lib/song.js
    También se ha modificado iv.yaml para añadir dicha entidad

---
+++ app-chores/lib/song.js
@@ -0,0 +1,13 @@
+class Song {
+	constructor(titulo, artista, disco, genero, duracion, archivo) {
+		this.titulo		= titulo;
+		this.artista	= artista;
+		this.genero		= genero; 
+		this.disco		= disco;
+		this.duracion	= duracion;
+		this.archivo	= archivo;
+	}
+}
+
+module.exports = Song 
+
