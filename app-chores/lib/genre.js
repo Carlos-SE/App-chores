ea56ac1f620ec7f0023be6c917239daba68bd00d
luistf24 <luis.tormo24@gmail.com>
2022-11-10 22:17:33 +0100

    fix: Elimino getters y atributo canciones en Genre
    Elimino getters de la clase Genre ya que en Javascript son automáticos.
    Elimino el atributo canciones para que un objeto Genre no cambie al ser creado.

--- app-chores/lib/genre.js
+++ app-chores/lib/genre.js
@@ -1,23 +1,11 @@
+
+const Song	= require('./song.js')
+
 class Genre {
-	constructor(genero, canciones) {
+	constructor(genero) {
 		this.genero		= genero;
-		this.canciones	= canciones;
 	}
 
-
-	/**
-	* @return: String con el género
- 	*/
-	getGenero() {
-		return this.genero;
-	}
-
-	/**
-	* @return: Listas de canciones asociadas al género
- 	*/
-	getCanciones() {
-		return this.canciones;
-	}
 }
 
 module.exports = Genre;
