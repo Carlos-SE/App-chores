7e05e13494f3d3886a7f90d7e7b029cb0736105a
Carlos-SE <carlossalaseiroa@gmail.com>
2023-01-22 19:45:48 +0100

    Closes #29 separando el algoritmo de recomendación del test

--- tests/recommedSong.test.js
+++ tests/recommedSong.test.js
@@ -1,32 +1,8 @@
 const Song = require('../lib/song')
 const Featuring = require('../lib/featuring')
 const Playlist = require('../lib/playlist')
+const recommendSong = require('../lib/functions')
 
-function recommendSong(playlist, song) {
-	const genresWeight = 75;
-	const featWeight = 50;
-
-	let sortedGenres = playlist.getSortedGenres();
-
-	let likely = 0;
-	let playlistLength = playlist.songs.length;
-	for (let genrePair of sortedGenres) {
-		let genre = genrePair[0];
-		if (song.genre === genre) {
-			likely += genrePair[1] / playlistLength * genresWeight;
-
-		}
-
-	}
-
-	if (playlist.hasFeaturing(song.interpretes)) {
-		likely += featWeight;
-
-	}
-
-	return likely;
-
-}
 
 test('Create a playlist and two separe songs to see if they are a good recommendation', () => {
 	let songs = [];
