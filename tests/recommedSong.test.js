cf538583713b9ae2d3c2e9cf66f3627824ce46e3
Carlos-SE <carlossalaseiroa@gmail.com>
2023-01-25 23:51:35 +0100

    Closes #44, eliminando la clase Featuring y adaptando el código a dicho cambio

--- tests/recommedSong.test.js
+++ tests/recommedSong.test.js
@@ -1,5 +1,4 @@
 const Song = require('../lib/song')
-const Featuring = require('../lib/featuring')
 const Playlist = require('../lib/playlist')
 const recommendSong = require('../lib/functions')
 
@@ -8,7 +7,7 @@ test('Create a playlist and two separe songs to see if they are a good recommend
 	let songs = [];
     let song1 = new Song("Feo Fuerte y Formal", "Loquillo Y Los Trogloditas", "Rock");
     let song2 = new Song("Venezia", "Hombres G", "Pop-Rock");
-    let collab3 = new Featuring(["Loquillo Y Los Trogloditas", "Fito y Fitipaldis"]);
+    let collab3 = ["Loquillo Y Los Trogloditas", "Fito y Fitipaldis"];
     let song3 = new Song("Luché contra la ley", collab3, "Rock");
 
     songs.push(song1, song2, song3);
