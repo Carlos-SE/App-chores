ad611f4f973f85f881f5797234be4e390e51aa5a
Carlos-SE <carlossalaseiroa@gmail.com>
2023-01-27 19:27:02 +0100

    Avanza #47, ajustándose a los nuevos cambios de la recomendaciones, añadiendo más ejemplos y comprobando que funciona correctamente

--- tests/recommedSong.test.js
+++ tests/recommedSong.test.js
@@ -9,23 +9,27 @@ test('Create a playlist and two separe songs to see if they are a good recommend
     let song2 = new Song("Venezia", "Hombres G", "Pop-Rock");
     let collab3 = ["Loquillo Y Los Trogloditas", "Fito y Fitipaldis"];
     let song3 = new Song("Luché contra la ley", collab3, "Rock");
+    let song4 = new Song("Antes de que cuente diez", "Fito y Fitipaldis", "Rock");
+    let song5 = new Song("Good Vibrations - Remastered 2001", "The Beach Boys", "Pop");
+    let song6 = new Song("Limbo", "Daddy Yankee", "Reggaeton");
+    let song7 = new Song("She's Not There", "The Zombies", "Pop-Rock");
+    let song8 = new Song("Esto no es Hawai(Qué wai)", "Loquillo Y Los Trogloditas", "Rock");
+    let song9 = new Song("Time of the Season", "The Zombies", "Psyco-Rock");
 
     songs.push(song1, song2, song3);
 
-    let suggestedSong1 = new Song("Antes de que cuente diez", "Fito y Fitipaldis", "Rock");
-    let suggestedSong2 = new Song("Good Vibrations - Remastered 2001", "The Beach Boys", "Pop");
-    let suggestedSong3 = new Song("Limbo", "Daddy Yankee", "Reggaeton");
-    let suggestedSong4 = new Song("She's Not There", "The Zombies", "Pop-Rock");
 
-    let suggestedSongs = [suggestedSong1, suggestedSong2, suggestedSong3, suggestedSong4];
+    let allSongs = [song1, song2, song3, song4, song5, song6, song7, song8, song9];
 
     let playlist = new Playlist(songs);
 
-    let recommended = recommendSong(playlist, suggestedSongs);
+    recommendSong(playlist, allSongs);
 
-    expect(recommended.includes(suggestedSong1)).toBe(true);
-    expect(recommended.includes(suggestedSong2)).toBe(false);
-    expect(recommended.includes(suggestedSong3)).toBe(false);
-    expect(recommended.includes(suggestedSong4)).toBe(false);
+    expect(playlist.songs.includes(song4)).toBe(true);
+    expect(playlist.songs.includes(song5)).toBe(false);
+    expect(playlist.songs.includes(song6)).toBe(false);
+    expect(playlist.songs.includes(song7)).toBe(false);
+    expect(playlist.songs.includes(song8)).toBe(true);
+    expect(playlist.songs.includes(song9)).toBe(false);
     
 })
