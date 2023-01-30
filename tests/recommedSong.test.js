4acae1b8653ec818ff4e6217e9846eabac102efa
Carlos-SE <carlossalaseiroa@gmail.com>
2023-01-30 13:06:03 +0100

    Closes #47, cambiando valor arbitrario para que se pueda pasar como un parámetro durantre la ejecución

--- tests/recommedSong.test.js
+++ tests/recommedSong.test.js
@@ -23,7 +23,7 @@ test('Create a playlist and two separe songs to see if they are a good recommend
 
     let playlist = new Playlist(songs);
 
-    recommendSong(playlist, allSongs);
+    recommendSong(playlist, allSongs, 3);
 
     expect(playlist.songs.includes(song4)).toBe(true);
     expect(playlist.songs.includes(song5)).toBe(false);
